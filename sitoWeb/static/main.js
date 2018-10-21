f33cb3cb2a7a3e130d06518bf7cef4e3cba92d53
Kaj Oskar Rusilowski <xedom08@gmail.com>
2018-10-22 00:10:43 +0200

    codice iniziale per un login semplice

--- sitoWeb/static/main.js
+++ sitoWeb/static/main.js
@@ -1,6 +1,12 @@
 const LISTA = document.querySelector('#lista');
 const NAMEINP = document.querySelector('#aggGioNome');
 const UIDINP = document.querySelector('#aggGioUID');
+const KEYINP = document.querySelector('#keyLoginSys');
 const ADDMODAL = document.querySelector('#aggiuntaGiocatore');
+
 const APIURI = 'http://95.89.230.39/api';
 reqGList(compRen,LISTA);
+
+function loginSimpleSystem(key) {
+    console.log(key.value)
+};
